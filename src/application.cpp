d906c5c88deb4f1cf65872f7074b1c6f0065ed42
Oleg Linkin <MaledictusDeMagog@gmail.com>
2016-07-07 02:04:47 +0300

    Add Application class - base class of Mnemosy

---
+++ src/application.cpp
@@ -0,0 +1,61 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "application.h"
+
+#include <QQuickView>
+
+#include "sailfishapp.h"
+
+namespace Mnemosy
+{
+    Application::Application(QObject *parent)
+    : QObject(parent)
+    , m_View(nullptr)
+    {
+    }
+
+    void Application::ShowUI()
+    {
+        if (!m_View)
+        {
+            qDebug() << "Construct view";
+            m_View = SailfishApp::createView();
+            m_View->setTitle("Mnemosy");
+            m_View->setSource(SailfishApp::pathTo("qml/harbour-mnemosy.qml"));
+            m_View->showFullScreen();
+        }
+        else
+        {
+            qDebug() << "Activating view";
+            m_View->raise();
+            m_View->requestActivate();
+        }
+    }
+
+    void Application::start()
+    {
+        ShowUI();
+    }
+} // namespace Mnemosy
