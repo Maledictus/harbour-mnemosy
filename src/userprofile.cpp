15f51cff243e7b844fb8e210edb3d8c03a99ac28
Oleg Linkin <MaledictusDeMagog@gmail.com>
2016-07-18 01:20:32 +0300

    Add UserProfile class

---
+++ src/userprofile.cpp
@@ -0,0 +1,185 @@
+/*
+The MIT License(MIT)
+
+Copyright(c) 2015 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "userprofile.h"
+#include <QtDebug>
+
+namespace Mnemosy
+{
+UserProfile::UserProfile(QObject* parent)
+: QObject(parent)
+, m_UserID(0)
+{
+}
+
+QUrl UserProfile::GetDefaultPicUrl() const
+{
+    return m_DefaultPicUrl;
+}
+
+void UserProfile::SetDefaultPicUrl(const QUrl& url)
+{
+    m_DefaultPicUrl = url;
+    emit avatarUrlChanged();
+}
+
+quint64 UserProfile::GetUserID() const
+{
+    return m_UserID;
+}
+
+void UserProfile::SetUserID(quint64 id)
+{
+    m_UserID = id;
+    emit userIdChanged();
+}
+
+QString UserProfile::GetUserName() const
+{
+    return m_UserName;
+}
+
+void UserProfile::SetUserName(const QString& name)
+{
+    m_UserName = name;
+    emit userNameChanged();
+}
+
+QString UserProfile::GetFullName() const
+{
+    return m_FullName;
+}
+
+void UserProfile::SetFullName(const QString& name)
+{
+    m_FullName = name;
+    emit fullNameChanged();
+}
+
+QStringList UserProfile::GetCommunities() const
+{
+    return m_Communities;
+}
+
+void UserProfile::SetCommunities(const QStringList& list)
+{
+    m_Communities = list;
+}
+
+QList<QPair<QString, QUrl>> UserProfile::GetAvatars() const
+{
+    return m_Avatars;
+}
+
+void UserProfile::SetAvatars(const QList<QPair<QString, QUrl>>& avatars)
+{
+    m_Avatars = avatars;
+}
+
+QDateTime UserProfile::GetBirthday() const
+{
+    return m_Birthday;
+}
+
+void UserProfile::SetBirthday(const QDateTime& dt)
+{
+    m_Birthday = dt;
+    return birthdayChanged();
+}
+
+QSet<FriendsGroup> UserProfile::GetFriendsGroups() const
+{
+    return m_Groups;
+}
+
+QSet<FriendsGroup>& UserProfile::GetFriendsGroups()
+{
+    return m_Groups;
+}
+
+void UserProfile::AddFriendsGroup(const FriendsGroup& group)
+{
+    m_Groups << group;
+}
+
+QByteArray UserProfile::Serialize() const
+{
+    quint16 ver = 1;
+    QByteArray result;
+    {
+        QDataStream ostr(&result, QIODevice::WriteOnly);
+        ostr << ver
+                << m_DefaultPicUrl
+                << m_UserID
+                << m_UserName
+                << m_FullName
+                << m_Communities
+                << m_Avatars;
+    }
+
+    return result;
+}
+
+UserProfile* UserProfile::Deserialize(const QByteArray& data, QObject *parent)
+{
+    quint16 ver = 0;
+    QDataStream in(data);
+    in >> ver;
+
+    if(ver > 1)
+    {
+        qWarning() << Q_FUNC_INFO
+                << "unknown version"
+                << ver;
+        return nullptr;
+    }
+
+    UserProfile *result = new UserProfile(parent);
+    in >> result->m_DefaultPicUrl
+            >> result->m_UserID
+            >> result->m_UserName
+            >> result->m_FullName
+            >> result->m_Communities
+            >> result->m_Avatars;
+
+    return result;
+}
+
+void UserProfile::UpdateProfile(UserProfile* profile)
+{
+    if (!profile)
+    {
+        return;
+    }
+
+    m_DefaultPicUrl = profile->GetDefaultPicUrl();
+    m_UserID = profile->GetUserID();
+    m_UserName = profile->GetUserName();
+    m_FullName = profile->GetFullName();
+    m_Communities = profile->GetCommunities();
+    m_Avatars = profile->GetAvatars();
+    m_Birthday = profile->GetBirthday();
+    m_Groups = profile->GetFriendsGroups();
+}
+} // namespace Mnemosy
---
+++ src/userprofile.h
@@ -0,0 +1,92 @@
+/*
+The MIT License(MIT)
+
+Copyright(c) 2016 Oleg Linkin <maledictusdemagog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <QDateTime>
+#include <QObject>
+#include <QSet>
+#include <QStringList>
+#include <QUrl>
+
+#include "src/friendsgroup.h"
+
+namespace Mnemosy
+{
+class UserProfile : public QObject
+{
+    Q_OBJECT
+
+    quint64 m_UserID;
+    QUrl m_DefaultPicUrl;
+    QString m_UserName;
+    QString m_FullName;
+    QStringList m_Communities;
+    QList<QPair<QString, QUrl>> m_Avatars;
+    QDateTime m_Birthday;
+    QSet<FriendsGroup> m_Groups;
+
+    Q_PROPERTY(quint64 userId READ GetUserID NOTIFY userIdChanged)
+    Q_PROPERTY(QUrl avatarUrl READ GetDefaultPicUrl NOTIFY avatarUrlChanged)
+    Q_PROPERTY(QString userName READ GetUserName NOTIFY userNameChanged)
+    Q_PROPERTY(QString fullName READ GetFullName NOTIFY fullNameChanged)
+    Q_PROPERTY(QDateTime birthday READ GetBirthday NOTIFY birthdayChanged)
+
+public:
+    explicit UserProfile(QObject *parent = 0);
+
+    QUrl GetDefaultPicUrl() const;
+    void SetDefaultPicUrl(const QUrl& url);
+    quint64 GetUserID() const;
+    void SetUserID(quint64 id);
+    QString GetUserName() const;
+    void SetUserName(const QString& name);
+    QString GetFullName() const;
+    void SetFullName(const QString& name);
+    QStringList GetCommunities() const;
+    void SetCommunities(const QStringList& list);
+    QList<QPair<QString, QUrl>> GetAvatars() const;
+    void SetAvatars(const QList<QPair<QString, QUrl>>& avatars);
+    QDateTime GetBirthday() const;
+    void SetBirthday(const QDateTime& dt);
+
+    QSet<FriendsGroup> GetFriendsGroups() const;
+    QSet<FriendsGroup>& GetFriendsGroups();
+    void AddFriendsGroup(const FriendsGroup& group);
+
+    QByteArray Serialize() const;
+    static UserProfile* Deserialize(const QByteArray& data, QObject *parent = 0);
+
+    void UpdateProfile(UserProfile *profile);
+
+signals:
+    void userIdChanged();
+    void avatarUrlChanged();
+    void userNameChanged();
+    void fullNameChanged();
+    void birthdayChanged();
+};
+} // namespace Mnemosy
+
+Q_DECLARE_METATYPE(Mnemosy::UserProfile*)
