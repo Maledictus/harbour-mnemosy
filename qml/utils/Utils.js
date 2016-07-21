4429256367328fa5b1d08a8c9daed34c4c53560b
Oleg Linkin <MaledictusDeMagog@gmail.com>
2016-07-21 09:06:16 +0300

    Add Utils to QML

---
+++ qml/utils/Utils.js
@@ -0,0 +1,22 @@
+.pragma library
+
+function generateDateString(dt) {
+    var today = new Date()
+    var date = "";
+    if (today.getDate() === dt.getDate() &&
+            today.getMonth() === dt.getMonth() &&
+            today.getFullYear() === dt.getFullYear()) {
+        date = qsTr("Today") + ", "
+    }
+    else if (today.getDate() === dt.getDate() + 1 &&
+            today.getMonth() === dt.getMonth() &&
+            today.getFullYear() === dt.getFullYear()){
+        date = qsTr("Yesterday") + ", "
+    }
+
+    if(date !== "") {
+        return date + " " + Qt.formatTime(dt, "hh:mm")
+    }
+
+    return Qt.formatDateTime(dt, "dd MMM yyyy hh:mm")
+}
